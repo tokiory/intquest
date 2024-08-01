373b24c8768252cc5d17d5522337a4c1205687a3
Vyacheslav Dmitriev <rtefforg@gmail.com>
2024-08-01 18:26:57 +0300

    feat(questions): database

---
+++ src/data/questions/backend/databaseCollection.ts
@@ -0,0 +1,91 @@
+import tags from "@/data/tags";
+import type {QuestionSection} from "@/types/question.types.ts";
+import {createQuestionSlug} from "@/utils/slug.ts";
+
+const slug = createQuestionSlug('database');
+
+export const slugs = {
+    differenceBetweenClusteredAndNonClustered: slug('difference-between-clustered-and-non-clustered'),
+    indexesMulticolumn: slug('indexes-multicolumn'),
+    isolationLevels: slug('indexes-multicolumn'),
+};
+
+const browserQuestionCollection: QuestionSection = {
+    title: 'Database',
+    icon: 'mdi:database',
+    collection: [
+        {
+            name: 'В чем разница между кластеризованным и некластеризованным индексами в SQL?',
+            slug: slugs.differenceBetweenClusteredAndNonClustered,
+            answer: [
+                'Кластеризованный индекс:',
+                'Определяет физический порядок данных в таблице.',
+                'Только один кластеризованный индекс на таблицу.',
+                'В MySQL, если первичный ключ не определен, в качестве кластеризованного индекса используется ' +
+                'первый уникальный индекс, в котором все столбцы являются NOT NULL.',
+                'Некластеризованный индекс:',
+                'Отдельная структура данных, хранящая значения индекса и указатели на фактические строки данных.',
+                'На одной таблице может быть несколько некластеризованных индексов.',
+                'Не определяет физический порядок данных.',
+                'Требует дополнительного шага для поиска фактических строк данных по сравнению с ' +
+                'кластеризованным индексом.'
+            ],
+            tags: [
+                tags.junior,
+            ],
+            references: [
+                {
+                    name: 'В чем разница между кластеризованным и некластеризованным индексами в SQL?',
+                    link: 'https://yandex.ru/q/datascience/10931953921/'
+                },
+            ]
+        },
+        {
+            name: 'Что такое составной индекс?',
+            slug: slugs.indexesMulticolumn,
+            answer: [
+                'Построенный с посыланием на несколько колонок одновременно. Иначе говоря, это комплексный индекс, ' +
+                'состоящий из нескольких колонок.'
+            ],
+            tags: [
+                tags.junior,
+            ],
+            references: [
+                {
+                    name: 'Postgres docs. Составные индексы',
+                    link: 'https://postgrespro.ru/docs/postgrespro/9.5/indexes-multicolumn'
+                },
+            ]
+        },
+        {
+            name: 'Уровни изолированности транзакций',
+            slug: slugs.isolationLevels,
+            answer: [
+                'Каждый уровень изолированности разрешает/запрещает определенные действия (возможности):',
+                'фантомное чтение — в рамках одной транзакции один и тот же запрос данных дает разные результаты, ' +
+                'что происходит из-за добавления данных другой (параллельной) транзакцией.',
+                'неповторяющееся чтение — в рамках одной транзакции один и тот же запрос данных дает разные ' +
+                'результаты, что происходит из-за изменения или удаления данных другой (параллельной) транзакцией.',
+                '«грязное» чтение — чтение данных, добавленных или измененных транзакцией, ' +
+                'которая впоследствии не откатится.',
+                'потерянное обновление — при одновременном изменении одного блока данных разными транзакциями ' +
+                'теряются все изменения, кроме последнего (похоже на “состояние гонки” в многопоточности).'
+            ],
+            tags: [
+                tags.middle,
+            ],
+            references: [
+                {
+                    name: 'Postgres docs. Изоляция транзакций',
+                    link: 'https://postgrespro.ru/docs/postgrespro/9.5/transaction-iso'
+                },
+                {
+                    name: 'Уровни изолированности транзакций для самых маленьких',
+                    link: 'https://habr.com/ru/articles/469415/'
+                },
+            ]
+        },
+    ]
+};
+
+export default browserQuestionCollection;
--- src/data/questions/backend/index.ts
+++ src/data/questions/backend/index.ts
@@ -1,9 +1,11 @@
 import python3Collection from './python3Collection.ts';
 import djangoCollection from './djangoCollection.ts';
 import sqlCollection from './sqlCollection.ts';
+import databaseCollection from "@/data/questions/backend/databaseCollection.ts";
 
 export default [
     python3Collection,
     djangoCollection,
+    databaseCollection,
     sqlCollection
 ]
