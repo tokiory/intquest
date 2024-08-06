638d22603cb3c546ee692715a264ce3c09ac1a9e
Vyacheslav Dmitriev <rtefforg@gmail.com>
2024-08-06 12:27:50 +0300

    feat(questions): postgres

--- src/data/questions/backend/index.ts
+++ src/data/questions/backend/index.ts
@@ -2,10 +2,12 @@ import python3Collection from './python3Collection.ts';
 import djangoCollection from './djangoCollection.ts';
 import sqlCollection from './sqlCollection.ts';
 import databaseCollection from "@/data/questions/backend/databaseCollection.ts";
+import postgresCollection from "@/data/questions/backend/postgresCollection.ts";
 
 export default [
     python3Collection,
     djangoCollection,
     databaseCollection,
+    postgresCollection,
     sqlCollection
 ]
